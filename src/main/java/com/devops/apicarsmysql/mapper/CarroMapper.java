7643037489c0458101738a88dc780af93e5d4371
Thiago Hernandes de Souza <thiago.hernandes@dbccompany.com.br>
2021-06-02 08:57:05 -0300

    Novos ajustes

--- src/main/java/com/devops/apicarsmysql/mapper/CarroMapper.java
+++ src/main/java/com/devops/apicarsmysql/mapper/CarroMapper.java
@@ -3,16 +3,7 @@ package com.devops.apicarsmysql.mapper;
 import com.devops.apicarsmysql.dto.CarroDto;
 import com.devops.apicarsmysql.entity.CarroEntity;
 
-import java.util.Objects;
-
 public class CarroMapper {
-
-    public static CarroEntity dtoToEntity(CarroDto carroDto) {
-        return CarroEntity.builder()
-                .id(!Objects.isNull(carroDto.getId()) ? carroDto.getId() : 0)
-                .marca(carroDto.getMarca())
-                .modelo(carroDto.getModelo()).build();
-    }
 
     public static CarroDto entityToDto(CarroEntity carroEntity) {
         return CarroDto.builder()
